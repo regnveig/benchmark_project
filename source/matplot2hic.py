bd1abc92ebfc9aa4672de2a72b185a3d164f96f4
BelokopytovaPS <belka2195@mail.ru>
2021-08-11 12:40:03 +0700

    initial commit

---
+++ source/matplot2hic.py
@@ -0,0 +1,128 @@
+import time
+import os, errno
+import subprocess
+import math
+import numpy as np
+import pandas as pd
+import pickle
+import matplotlib.pyplot as plt
+from matrix_plotter import MatrixPlotter
+from termcolor import colored
+from shared import  get_bin_size
+
+
+#   This func is for visualising Predictor results.
+#
+#   matplot_obj             Matrix_Plotter Object
+#   fname                   name of current visualisation
+#   out_folder              folder where all the visualisation files are
+#   juicer_path             path to juicer .jar-file
+#   ----------------------
+#
+#   Love, Emil
+
+def MatPlot2HiC(matplot_obj, fname, out_folder, juicer_path=None):
+    def Pandas2ChrSizes(chrsizes_filename,
+                        pandas_df):  # This func takes all the chromosomes from pandas object, find out their sizes and write into file
+        chromosomes = pandas_df.ix[:, 0].unique()
+        chrsizes_table = pd.DataFrame(columns=chromosomes)
+
+        for i in range(len(chromosomes)):
+            buf = pandas_df.loc[pandas_df['chr'] == chromosomes[i]][['contact_st', 'contact_en']]
+            max1 = buf.max().max()
+            chrsizes_table.at[0, chromosomes[i]] = max1
+
+            print('Completed: {}%'.format(i * 100 // len(chromosomes)), end='\r')
+
+        chr_list = list(chrsizes_table)
+
+        chrsizes_file = open(chrsizes_filename, 'w')
+
+        for j in range(len(chr_list)):
+            chrsizes_file.write(chr_list[j] + '\t' + str(chrsizes_table.iloc[0][chr_list[j]]) + '\n')
+
+        chrsizes_file.close()
+
+    def Pandas2Pre(pre_filename, pandas_df):  # This func makes pre-HiC file from the pandas object, control or data
+        pre_file = open(pre_filename, 'w')
+        data_rows = pandas_df.shape[0]
+
+        pandas_df.columns = ["chr1", "start", "end", "count"]
+        pandas_df['str1'] = 0
+        assert len(pandas_df.loc[(pandas_df['count'] < 0.000001) & (pandas_df['count'] != 0)]) < (len(pandas_df['count']) / 10)
+        pandas_df['exp'] = pandas_df['count'] * ( 1000000 )
+        pandas_df['exp'] = round(pandas_df['exp']).astype(int)
+
+        pandas_df.to_csv(pre_file, sep=" ",
+                         columns=['str1', 'chr1', 'start', 'start', 'str1', 'chr1', 'end', 'end', 'exp'], header=False,
+                         index=False,
+                         line_terminator="\n")
+
+        pre_file.close()
+
+    # make dirs
+    try:
+        os.makedirs(out_folder + '/' + fname)
+        os.makedirs(out_folder + '/' + fname + '/pre')
+        os.makedirs(out_folder + '/' + fname + '/hic')
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+    # make filenames
+    chromsizes_filename = out_folder + '/' + fname + '/pre/chrom.sizes'
+    pre_data_filename = out_folder + '/' + fname + '/pre/pre_data.txt'
+    hic_data_filename = out_folder + '/' + fname + '/hic/data.hic'
+    pre_control_filename = out_folder + '/' + fname + '/pre/pre_control.txt'
+    hic_control_filename = out_folder + '/' + fname + '/hic/control.hic'
+
+    # make chrom.sizes, pre-Hic for data and control
+    print('Make chromosome sizes file...')
+    time1 = time.time()
+    Pandas2ChrSizes(chromsizes_filename, matplot_obj.data)
+    time2 = time.time()
+    print('Time: ' + str(round(time2 - time1, 3)) + ' sec\n')
+    print(colored("[SUCCESS]", 'green') + ' Chromosome sizes file created.\n')
+
+    print('Make data pre-HiC file...')
+    time1 = time.time()
+    Pandas2Pre(pre_data_filename, matplot_obj.data)
+    time2 = time.time()
+    print('Time: ' + str(round(time2 - time1, 3)) + ' sec\n')
+    print(colored("[SUCCESS]", 'green') + ' DATA pre-HiC file created.\n')
+
+    print('Make control pre-HiC file...')
+    time1 = time.time()
+    Pandas2Pre(pre_control_filename, matplot_obj.control)
+    time2 = time.time()
+    matplot_obj.columns = ["chr1", "start", "end", "count"]
+    binsize = str(get_bin_size(matplot_obj.control, fields=["start", "start"]))
+    print('Time: ' + str(round(time2 - time1, 3)) + ' sec\n')
+    print(colored("[SUCCESS]", 'green') + ' CONTROL pre-HiC file created.\n')
+
+    #call juicer
+    if juicer_path is None:
+        juicer_path = os.path.join(os.path.dirname(os.path.abspath(os.path.abspath(__file__))),
+                                   "juicer_tools.jar")
+    cmd =  ['java', '-jar', juicer_path, 'pre', pre_data_filename, hic_data_filename, chromsizes_filename, '-n',
+         '-r', binsize]
+    print("Running command:")
+    print (" ".join(map(str,cmd)))
+    try:
+        subprocess.check_output(cmd)
+    except subprocess.CalledProcessError as e:
+        print (e.output)
+        raise Exception()
+    print(colored("[SUCCESS]", 'green') + ' DATA HiC file created.\n')
+
+    cmd = ['java', '-jar', juicer_path, 'pre', pre_control_filename, hic_control_filename, chromsizes_filename,
+         '-n', '-r', binsize]
+    print("Running command:")
+    print (" ".join(map(str,cmd)))
+    try:
+        subprocess.check_output(cmd)
+    except subprocess.CalledProcessError as e:
+        print (e.output)
+        raise Exception()
+
+    print(colored("[SUCCESS]", 'green') + ' CONTROL HiC file created.\n')
---
+++ source/matrix_plotter.py
@@ -0,0 +1,109 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import logging
+import numpy as np
+from shared import get_bin_size
+
+class MatrixPlotter(): #The class that plot fragments of heatmap
+                        #Note - not optimized for large datasets such as whole chromosome
+    def process(self,data):
+        return data.loc[:,["chr","contact_st","contact_en","contact_count"]]
+
+    def set_control(self,ctrl):
+        self.control = self.process(ctrl)
+
+    def read_control(self,ctrl_fname):
+        self.control = self.process(pd.read_csv(ctrl_fname,delimiter="\t"))
+
+    def set_data(self,data):
+        self.data = self.process(data)
+
+    def read_data(self,data_fname):
+        self.data = self.process(pd.read_csv(data_fname,delimiter="\t"))
+
+    def convert2binned(self, data, interval, binsize):
+        data["contact_st_bin"] = data["contact_st"].apply(lambda x: (x - interval.start) // binsize)
+        data["contact_en_bin"] = data["contact_en"].apply(lambda x: (x - interval.start) // binsize)
+        return data
+
+    # def set_apply_log(self, apply_log):
+    #     self.apply_log = apply_log
+
+    def getMatrix4plot(self, interval, binsize = None):
+        def appendSeries2matrix(x,matrix,triangle = "both"): #x - series to append into matrix
+            if triangle == "both":
+                matrix[x["contact_en_bin"], x["contact_st_bin"]] = x["contact_count"]
+                matrix[x["contact_st_bin"], x["contact_en_bin"]] = x["contact_count"]
+            elif triangle == "upper":
+                matrix[x["contact_st_bin"], x["contact_en_bin"]] = x["contact_count"]
+            elif triangle == "lower":
+                matrix[x["contact_en_bin"], x["contact_st_bin"]] = x["contact_count"]
+            else:
+                raise
+
+        try:
+            self.data
+        except:
+            logging.error("Please provide the data first")
+            return
+
+        if binsize == None: #infer binsize from data
+            # dist = pd.unique(self.data["contact_en"]-self.data["contact_st"])
+            # sorted_starts = np.sort(self.data["contact_st"].values[:min(1000,len(self.data))])
+            # dist2 = np.unique(np.subtract(sorted_starts[1:],sorted_starts[:-1]))
+            # assert (dist2 >= 0).all()
+            # dist = np.unique(np.concatenate((dist,dist2)))
+            # dist = dist[np.nonzero(dist)]
+            # assert len(dist) > 0
+            # binsize = min(dist)
+            binsize = get_bin_size(self.data)
+            logging.getLogger(__name__).info("Using binsize "+str(binsize))
+
+        Interval_size_bins = (interval.end - interval.start) // binsize + 1
+        matrix = np.zeros(shape=(Interval_size_bins, Interval_size_bins))
+        data = self.data.query("@interval.start <= contact_st <= @interval.end &"
+                               "@interval.start <= contact_en <= @interval.end")
+        data = self.convert2binned(data, interval, binsize)
+
+        try:
+            len(self.control)
+            with_control = True
+        except:
+            with_control = False
+
+        if with_control:
+            logging.getLogger(__name__).debug("Running with control")
+            control = self.control.query("@interval.start <= contact_st <= @interval.end &"
+                                   "@interval.start <= contact_en <= @interval.end")
+            control = self.convert2binned(control, interval, binsize)
+            data.apply(appendSeries2matrix,matrix=matrix,triangle="upper",axis = "columns")
+            control.apply(appendSeries2matrix,matrix=matrix,triangle="lower",axis = "columns")
+        else:
+            data.apply(appendSeries2matrix,matrix=matrix,triangle="both",axis = "columns")
+
+        #remember values for future operations
+        self.matrix = matrix
+        self.binsize = binsize
+        self.interval_size_bins = Interval_size_bins
+        self.interval = interval
+
+        return matrix
+
+    def get_bins_strart_labels(self, maxTicksNumber = 1000000):
+        try:
+            self.matrix
+        except:
+            logging.error("Please compute matrix first")
+            return None
+
+        pos = []
+        labels = []
+        curr_pos = 0
+        increment = self.interval_size_bins // maxTicksNumber
+        logging.getLogger(__name__).debug(str(increment))
+        logging.getLogger(__name__).debug(str(self.interval_size_bins))
+        while curr_pos <= self.interval_size_bins:
+            labels.append(str(self.interval.chr) + ":" + str((self.interval.start + curr_pos*self.binsize) // 1000) + "K")
+            pos.append(curr_pos)
+            curr_pos += increment
+        return pos,labels
